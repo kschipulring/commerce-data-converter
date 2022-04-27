8c809b175276841229824ca2e3b8de30abd5b1b7
kschipulring <karl@3ringprototype.com>
2022-04-27 15:48:46 -0500

    CONV-2 - more Config class parameters

--- src/main/java/com/migrator/Config.java
+++ src/main/java/com/migrator/Config.java
@@ -19,7 +19,18 @@ public class Config {
     how many records per page? For the orders call, will be the number of orders
     that could return. Ditto for customers, etc.
     */
-    public static int mage_max_per_page = 10;
+    public static int mage_max_per_page;
+
+    //what is the parent directory for all saved files?
+    public static String base_save_dir;
+
+    //which child directory does the JSON from the Magento API get saved in?
+    public static String json_save_subdir = "json_src";
+
+    //where do the XML files that will be sent to FTP be saved for now?
+    public static String xml_save_subdir = "xml_dest";
+
+    public static int http_duration_wait;
 
     // Constructor
     // Here we will be creating private constructor restricted to this class itself
@@ -40,9 +51,16 @@ public class Config {
         //Magento REST API security basic auth token
         mage_auth_token = dotenv.get("MAGE_AUTH_TOKEN");
 
+        //Magento REST API base URL
         mage_api_base_url = dotenv.get("MAGE_API_BASE_URL");
 
-        mage_max_per_page = Integer.parseInt( dotenv.get("MAGE_MAX_PER_PAGE") );
+        base_save_dir = dotenv.get("BASE_SAVE_DIR", "saved_files");
+
+        //how many results per page maximum?
+        mage_max_per_page = Integer.parseInt( dotenv.get("MAGE_MAX_PER_PAGE", "10") );
+
+        //how long should the API be waited for?
+        http_duration_wait = Integer.parseInt( dotenv.get("HTTP_DURATION_WAIT", "10") );
     }
 
     // Static method to create instance of Singleton class
