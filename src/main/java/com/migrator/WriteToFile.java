6b130d8f0cfeb03c59479b3c257a02812ed804c3
kschipulring <karl@3ringprototype.com>
2022-04-27 16:45:13 -0500

    CONV-2 - WriteFile class updates, including handling either null / empty filenames or proposed file contents.  Also handling for when desired subdirectories do not yet exist.

--- src/main/java/com/migrator/WriteToFile.java
+++ src/main/java/com/migrator/WriteToFile.java
@@ -1,18 +1,35 @@
 package com.migrator;
 
+import java.io.File;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 
 public class WriteToFile {
     public static void write(String f_name, String f_contents) {
 
+        //the filename must not be empty or null.
+        if( f_name == null || f_name.isEmpty() || f_contents == null || f_contents.isEmpty() )
+        {
+            return;
+        }
+
         //which is the base folder of this app?
         String abs_path = new java.io.File("").getAbsolutePath();
 
+        String directory = abs_path + "/" + Config.base_save_dir + "/";
+        String file_write_name = directory + "/" + f_name;
+
+        //System.out.println( "file_write_name = " + file_write_name );
+
         try {
-            String write_name = abs_path + "/saved_files/" + f_name;
+
+            //Java only plays nice when the desired directory for the file already exists
+            File myFile = new File(file_write_name);
+            if(!myFile.getParentFile().exists()) {
+                myFile.getParentFile().mkdirs();
+            }
             
-            FileWriter myWriter = new FileWriter(write_name);
+            FileWriter myWriter = new FileWriter(file_write_name);
             myWriter.write(f_contents);
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
