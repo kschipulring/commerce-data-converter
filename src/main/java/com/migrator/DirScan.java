e537a7ac5423d7cf4bfb8fb229532875ef78d9ee
kschipulring <karl@3ringprototype.com>
2022-04-27 08:12:49 -0500

    CONV-2 - a tool to find out if a file starting with a certain pattern already exists.  If so, return the full path name of the first match.

---
+++ src/main/java/com/migrator/DirScan.java
@@ -0,0 +1,42 @@
+package com.migrator;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class DirScan
+{
+
+    public static void main(String[] args){
+
+        System.out.println( fileStartsWithFullName("orders_") );
+    }
+
+    /*
+    find out if a file starting with a certain pattern already exists.
+    If so, return the full path name of the first match.
+    */
+    public static String fileStartsWithFullName(String fname_starts_with)
+    {
+        
+        //start with the root folder of this Java app.
+        String abs_path = new java.io.File("").getAbsolutePath();
+        
+        File root = new File(abs_path + "/saved_files/");
+        FilenameFilter beginswithm = new FilenameFilter()
+        {
+            public boolean accept(File directory, String filename) {
+                return filename.startsWith(fname_starts_with);
+            }
+        };
+
+        File[] files = root.listFiles(beginswithm);
+        for (File f: files)
+        {
+
+            //we only care about the first match
+            return f.toString();
+        }
+
+        return null;
+    }
+}
