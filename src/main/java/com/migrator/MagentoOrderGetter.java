bc199834ffa5bcb07b40385058a927ef417f1ca5
kschipulring <karl@3ringprototype.com>
2022-04-28 08:12:17 -0500

    CONV-2 - MagentoOrderGetter class - used directly to get the JSON

---
+++ src/main/java/com/migrator/MagentoOrderGetter.java
@@ -0,0 +1,30 @@
+package com.migrator;
+import java.io.IOException;
+
+//import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MagentoOrderGetter extends MagentoDataGetter{
+
+    protected JSONObject jsonObject = null;
+    
+    //constructor
+    public MagentoOrderGetter() throws IOException { }
+
+    public JSONObject getOrdersJson(int... cp) throws IOException, InterruptedException
+    {
+        
+        //if no specified current page, start it with zero
+        int current_page = cp.length > 0 ? cp[0] : this.current_page;
+
+        //overridden from the parameter
+        this.current_page = current_page;
+
+        //from either the REST API or the 'saved_files' cache
+        String json_str = this.getJSONstring("orders");
+
+        JSONObject jsonObject = new JSONObject( json_str );
+
+        return jsonObject;
+    }
+}
