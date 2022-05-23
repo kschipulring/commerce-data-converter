fa313e6e55ac35ab5730418af56b479e95416d4d
kschipulring <karl@3ringprototype.com>
2022-05-23 09:16:36 -0500

    CONV-2 - use of javax.annotation @Nullable.  Used in class JSONToXML constructor

--- src/main/java/com/migrator/JSONToXML.java
+++ src/main/java/com/migrator/JSONToXML.java
@@ -1,22 +1,50 @@
 package com.migrator;
 
 import java.io.IOException;
+import java.io.File;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
+
+import javax.annotation.*;
  
 public class JSONToXML {
-    public static void main(String...s) throws IOException{
 
+    //default
+    public String section = "orders";
+
+    public Integer mage_max_per_page = 5;
+
+    public String abs_path = "";
+
+    //your company name
+    public static String company_name = "";
+
+    public JSONToXML( @Nullable String section ) throws IOException{
+
+        section = section != null ? section : this.section;
+
+        this.section = section;
+        
         //make sure the Config instance is loaded
         Config.getInstance();
 
         //which is the base folder of this app?
-        String abs_path = new java.io.File("").getAbsolutePath();
+        this.abs_path = new java.io.File("").getAbsolutePath();
+
+        //this.section = section_override != null ? section_override : this.section;
+
+        
+
+        System.out.println( "please allow me to introduce myself with = " + this.section );
+    }
+
+    public static void main(String...s) throws IOException{
+
+        JSONToXML jsonToXML = new JSONToXML(null);
 
 
-        String json_folder = abs_path + "/" + Config.base_save_dir + "/" + Config.json_save_subdir + "/orders/";
+        String json_folder = jsonToXML.abs_path + "/" + Config.base_save_dir + "/" + Config.json_save_subdir + "/orders/";
         String xml_folder = Config.xml_save_subdir + "/orders/";
 
         String json_filename = json_folder + "tester.json";
