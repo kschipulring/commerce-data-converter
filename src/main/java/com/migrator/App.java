4fad8c9d095473eb0223d1e4b1b7aefe02e5b03b
kschipulring <karl@3ringprototype.com>
2022-05-23 17:03:25 -0500

    CONV-2 - main class cleanup for basic ops like saving the JSON, then converting it into XML

--- src/main/java/com/migrator/App.java
+++ src/main/java/com/migrator/App.java
@@ -18,10 +18,11 @@ public class App
     gets Magento orders from the specified environment via REST, then saves them
      as .json files in specified destination.
     */
-    public static void getMageOrders( HashMap<String, String> props) throws IOException, InterruptedException
+    public static JSONArray getMageOrders( HashMap<String, String> props) throws IOException, InterruptedException
     {
         
-        //page_size=234,env=mcstaging,page_start=5,page_range=5-35
+        // default value for the command line props, which is empty HashMap.
+        props = props != null ? props : new HashMap<String, String>();
 
         /*
         which page of results? Best off starting with '1' as it is the same as zero.
@@ -31,16 +32,19 @@ public class App
                             Integer.parseInt( props.get("page_start") ) : 1;
 
         //how many results per page?
-        Integer page_size = props.containsKey("page_size") ? 
-                            Integer.parseInt( props.get("page_size") ) : 10;
+        Integer max_per_page = props.containsKey("max_per_page") ? 
+                            Integer.parseInt( props.get("max_per_page") ) : 10;
         
-        MagentoOrderGetter mog = new MagentoOrderGetter();
+        MagentoOrderGetter mog = new MagentoOrderGetter(max_per_page);
 
         //save the JSON files after downloading content from Magento API?
         mog.save_file = true;
 
         //rely on existing saved JSON files if available?
         mog.load_file_if_exist = true;
+
+        //which page of results to start off with?
+        mog.current_page = page_start;
 
         /*
         page_start is fed as a method parameter, because I find that easier for 
@@ -51,22 +55,45 @@ public class App
         JSONArray jsonArray = orders_json.getJSONArray("items");
 
         System.out.println( jsonArray.getJSONObject(0).get("created_at") );
+
+        return jsonArray;
     }
 
-    public void saveSForders(){}
+    public static void saveSForders(JSONArray mage_orders) throws IOException{
+
+        //System.out.println( mage_orders );
+        String start_ts = mage_orders.getJSONObject(0).getString("created_at");
+
+        JSONObject mage_order_obj = new JSONObject();
+
+
+        System.out.println( "start_ts = " + start_ts );
+        
+
+        mage_order_obj.put("items", mage_orders);
+
+
+        Mage2SF mage2SF = new Mage2SF();
+
+        JSONObject sf_data = mage2SF.mage2SFObj( mage_order_obj );
+
+        mage2SF.prepareSFXMLFile(start_ts, sf_data);
+/**/
+        //System.out.println( sf_data );
+    }
 
     /*
     get the command line parameters (assuming that there are any)
 
     The Format for the args. All parameters are optional and do not need to be in any order.
-    env:mcstaging,page_size:234,page_start:5,page_range:5-35,mode=get|convert|sftp|all
+    env:mcstaging,page_size:234,page_start:5,page_end:35,mode:get|getconvertxml|getconvertcsv|convertxml|convertcsv|sftpxml|sftpcsv
 
-    NOTE: parameter 'mode' sample above is only one of the possibilites 
+    NOTE: parameter 'mode' value sample above is only one of the possibilites 
     seperated by the bar character.
     */
     protected static HashMap<String, String> getParams(String[] args){
 
-        HashMap<String, String> cl_props = null;
+        HashMap<String, String> cl_props = new HashMap<String, String>();
 
         String line = null;
 
@@ -74,10 +101,11 @@ public class App
             line = args[0];
         }
 
+        //only process if there are actual command line parameters
         if(line != null && line.length() > 3 ) {
-            cl_props = new HashMap<String, String>();
 
-            String str[] = line.split(",");
+            //split the key/value pairs by either commas or ampersands
+            String str[] = line.split(",|&");
 
             for(int i=0; i<str.length; i++){
                 String arr[] = str[i].split(":|=");
@@ -95,12 +123,49 @@ public class App
         // get the command line parameters (assuming that there are any)
         HashMap<String, String> cl_props = getParams( args );
 
-        System.out.println( cl_props );
-        
-        /*getMageOrders( args );
+        //mode=get|getconvertxml|getconvertcsv|convertxml|convertcsv|sftpxml|sftpcsv
+        String mode = "";
+        String xyz =  null;
 
-        for (String s: args) {
-            //System.out.println(s);
+        System.out.println( cl_props );
+
+        if( cl_props.containsKey("mode") ){
+            System.out.println( cl_props.containsKey("mode")  );
+        }
+        
+        if( cl_props.containsKey("mode") ){
+            mode = cl_props.get("mode");
+        }
+
+        JSONArray mage_orders = null;
+
+        switch (mode) {
+            case "convertxml":
+                
+            
+            break;
+            case "get":
+                mage_orders = getMageOrders( cl_props );
+
+            break;
+            case "getconvertxml":
+                mage_orders = getMageOrders( cl_props );
+
+                saveSForders(mage_orders);
+            default:
+                mage_orders = getMageOrders( cl_props );
+            
+                
+            break;
+        }
+
+        System.out.println( "xyz = " + xyz );
+    
+
+        //saveSForders(mage_orders);
+
+        /*for (String s: args) {
+            System.out.println(s);
         }*/
 
         //items.map().forEach((k, v) -> System.out.println(k + ":" + v));
