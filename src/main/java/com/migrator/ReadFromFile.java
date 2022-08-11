99f207bea9caaedf5c3516b616f0ac2681b9a973
kschipulring <karl@3ringprototype.com>
2022-08-11 14:15:22 -0500

    CONV-3+CONV-2 - logging of file read attempts as well as subsequent failure messages.

--- src/main/java/com/migrator/ReadFromFile.java
+++ src/main/java/com/migrator/ReadFromFile.java
@@ -8,6 +8,9 @@ import java.util.stream.Stream;
 
 public class ReadFromFile {
     public static String contents(String file_name){
+
+        M2SLogger.info("ReadFromFile file_name = " + file_name);
+
         try (Stream<String> lines = Files.lines(Paths.get(file_name))) {
     
             // UNIX \n, WIndows \r\n
@@ -16,6 +19,9 @@ public class ReadFromFile {
             return content;
         } catch (IOException e) {
             e.printStackTrace();
+
+            M2SLogger.severe("ReadFromFile file_name = " + file_name + " failed");
+            M2SLogger.severe(e.getMessage());
         }
 
         return null;
