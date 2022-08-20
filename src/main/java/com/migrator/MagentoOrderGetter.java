330dd917982d765b2b9e23fefd47ec33dd8eed16
kschipulring <karl@3ringprototype.com>
2022-08-20 13:40:22 -0500

    CONV-3, CONV-2 - fixes to Magento orders endpoint.

--- src/main/java/com/migrator/MagentoOrderGetter.java
+++ src/main/java/com/migrator/MagentoOrderGetter.java
@@ -1,9 +1,15 @@
 package com.migrator;
 import java.io.IOException;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import javax.annotation.Nullable;
 
-//import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MagentoOrderGetter extends MagentoDataGetter{
@@ -21,10 +27,42 @@ public class MagentoOrderGetter extends MagentoDataGetter{
         endpoint_extras += "&searchCriteria[filter_groups][0][filters][0][value]=0";
         endpoint_extras += "&searchCriteria[filter_groups][0][filters][0][condition_type]=eq";
 
-        //must at least have a populated increment_id field
-        endpoint_extras += "&searchCriteria[filter_groups][1][filters][0][field]=increment_id";
+        //must at also have a customer id
+        endpoint_extras += "&searchCriteria[filter_groups][1][filters][0][field]=customer_id";
         endpoint_extras += "&searchCriteria[filter_groups][1][filters][0][value]=null";
         endpoint_extras += "&searchCriteria[filter_groups][1][filters][0][condition_type]=neq";
+
+        //must at least have a populated increment_id field
+        endpoint_extras += "&searchCriteria[filter_groups][2][filters][0][field]=increment_id";
+        endpoint_extras += "&searchCriteria[filter_groups][2][filters][0][value]=null";
+        endpoint_extras += "&searchCriteria[filter_groups][2][filters][0][condition_type]=neq";
+
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        
+        //date from 18 months before present
+        Date today = new Date();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(today);
+        cal.add(Calendar.MONTH, -18);
+        Date prior18months = cal.getTime();
+        System.out.println(prior18months);
+        
+        //18 months ago string
+        String dateStrFrom = dateFormat.format(prior18months);
+        System.out.println("result is " + dateStrFrom);
+
+        //today string
+        String dateStrTo = dateFormat.format(today);
+        System.out.println("result today is " + dateStrTo);
+
+        endpoint_extras += "&searchCriteria[filter_groups][3][filters][0][field]=created_at";
+        endpoint_extras += "&searchCriteria[filter_groups][3][filters][0][value]=" + dateStrFrom;
+        endpoint_extras += "&searchCriteria[filter_groups][3][filters][0][condition_type]=from";
+
+        endpoint_extras += "&searchCriteria[filter_groups][4][filters][0][field]=created_at";
+        endpoint_extras += "&searchCriteria[filter_groups][4][filters][0][value]=" + dateStrTo;
+        endpoint_extras += "&searchCriteria[filter_groups][4][filters][0][condition_type]=to";
 
         //whitelist of returned fields for orders
         endpoint_extras += "&items[entity_id,base_currency_code,base_discount_amount,created_at,";
@@ -41,6 +79,9 @@ public class MagentoOrderGetter extends MagentoDataGetter{
         endpoint_extras += "base_shipping_tax_amount,base_shipping_incl_tax,subtotal,";
         endpoint_extras += "tax_amount,total_due,payment_additional_info,";
         endpoint_extras += "payment[cc_exp_month,cc_exp_year,amount_ordered,method],shipping_description]";
+
+        //want to know how many possible records are available, across all possible pages of results
+        endpoint_extras += ",total_count";
 
         this.endpoint_extras = endpoint_extras;
     }
