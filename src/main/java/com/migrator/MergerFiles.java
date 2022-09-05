4b78bec02a62feb2a18b6d14de9ff9025682da6f
kschipulring <karl@3ringprototype.com>
2022-09-05 07:12:55 -0500

    CONV-3, CONV-2 - making the merge file class work with the zip file class

--- src/main/java/com/migrator/MergerFiles.java
+++ src/main/java/com/migrator/MergerFiles.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.annotation.Nullable;
  
@@ -28,27 +29,33 @@ public class MergerFiles {
           folderFiles2MergedFiles(files_source_folder, null, "LegacyOrder_", "LegacyOrderItem_");
 	}
 
+     //to merge all files with the same prefix and filetype in a directory
      public static void folderFiles2MergedFiles(
           String files_source_folder,
           @Nullable String save_subdir,
           @Nullable String... file_prefixes
-     ) throws NullPointerException
+     ) throws NullPointerException, IOException
      {
 
+          //default subdirectory in case not an actual string
           if(save_subdir == null){
                save_subdir = "LegacyOrders";
+          }else{
+               save_subdir = save_subdir.trim();
           }
 
+          //no point in continuing without even a prefix of a filename to scan for.
           if(file_prefixes == null){
-               System.out.println("viva las vegas");
+               M2SLogger.warning("no apparent files present");
 
                return;
           }
 	
+          //scan the directory for filenames
           File folder = new File(files_source_folder);
           File[] listOfFiles = folder.listFiles();
 
-          //if there are no files here, then what is the point?
+          //if there are no actual files here, then what is the point?
           if(listOfFiles == null){
                return;
           }
@@ -59,10 +66,13 @@ public class MergerFiles {
           HashMap<String, ArrayList<String>> file_prefix_lists = new HashMap<String, ArrayList<String>>();
 
           for (String file_prefix : file_prefixes) {
-               file_prefix_lists.put(file_prefix, new ArrayList<String>() );
+               file_prefix_lists.put(file_prefix, new ArrayList<String>());
           }
+
+          //this will be the basename for the zip file. It will come from the first filename of the first collection.
+          String first_filename = null;
      
-          //loop through all the files in this directory
+          //loop through all the files in this directory. Add a file if it qualifies
           for (int i = 0; i < listOfFiles.length; i++) {
                if (listOfFiles[i].isFile()) {
                     
@@ -71,28 +81,42 @@ public class MergerFiles {
                          if( listOfFiles[i].getName().contains(file_prefix) ){
                               //add the filename to its respective list
                               file_prefix_lists.get(file_prefix).add( listOfFiles[i].getName() );
+
+                              //shall fire only once
+                              if(first_filename == null){
+                                   first_filename = listOfFiles[i].getName();
+
+                                   M2SSystem.println( "first_filename = " + first_filename );
+                              }
                          }
                     }
                }
           }
 
-          M2SSystem.println( file_prefix_lists.get( "LegacyOrder_" ) );
+          //will be populated with only the end merged files
+          List<String> srcFiles = new ArrayList<String>();
 
-
-          System.out.println( file_prefix_lists.get( "LegacyOrder_" ).size() );
-
-
+          //now merge the file collection types into combined masters
           for (String file_prefix : file_prefixes) {
                File[] temp_files = new File[ file_prefix_lists.get(file_prefix).size() ];
           
+               //loop through the contents of all the files that meet the criteria
                for(int i = 0; i < file_prefix_lists.get(file_prefix).size(); i++ ){
                     temp_files[i] = new File( files_source_folder + file_prefix_lists.get(file_prefix).get(i) );
                }
+
+               String mergedFileExtension = getFnExtension(first_filename);
+
+               M2SSystem.println( "mergedFileExtension = " + mergedFileExtension );
           
-               String mergedFilePath = files_source_folder + File.separator + save_subdir + File.separator + file_prefix + ".csv";
+               //what shall be the merged file name?
+               String mergedFilePath = files_source_folder + File.separator + save_subdir;
+               mergedFilePath += File.separator + file_prefix + "." + mergedFileExtension;
+
+               srcFiles.add(mergedFilePath);
           
                File mergedFile = new File(mergedFilePath);
-          
+
                if(!mergedFile.getParentFile().exists()) {
                     mergedFile.getParentFile().mkdirs();
                }
@@ -100,6 +124,11 @@ public class MergerFiles {
                mergeFiles(temp_files, mergedFile);
           }
 
+          //final zip file name
+          String dest_zipfile_name = files_source_folder + File.separator + removeFnExtension(first_filename) + ".zip";
+
+          //zip the merged files
+          FilesZipper.zipFilesUp(srcFiles, dest_zipfile_name);
 
      }
  
@@ -142,4 +171,26 @@ public class MergerFiles {
 		}
  
 	}
+
+     public static String getFnExtension(String fileName) {
+          String extension = "";
+          
+          int index = fileName.lastIndexOf('.');
+          if (index > 0) {
+              extension = fileName.substring(index + 1);
+          }
+          
+          return extension;
+     }
+
+     //for removing an extension from a filename. So 'test123.csv' => 'test123'
+     public static String removeFnExtension(String fileName) {
+          int extPos = fileName.lastIndexOf(".");
+          
+          if(extPos == -1){
+              return fileName;
+          }else{
+              return fileName.substring(0, extPos);
+          }
+     }
 }
