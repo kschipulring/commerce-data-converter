c34f50cd3548890739e99473a0d9666a893d954e
kschipulring <karl@3ringprototype.com>
2022-09-22 11:00:12 -0500

    CONV-3 - more zip file testing, inconsequential.

--- src/main/java/com/migrator/FilesZipper.java
+++ src/main/java/com/migrator/FilesZipper.java
@@ -3,7 +3,6 @@ package com.migrator;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipOutputStream;
 import java.util.ArrayList;
@@ -18,7 +17,7 @@ public class FilesZipper {
           System.out.println( Config.base_save_dir );
 
           String srcFile = Config.base_save_dir + File.separator + "test1.txt";
-          String dest_zipfile_name = Config.base_save_dir + File.separator + "test1.txt";
+          String dest_zipfile_name = Config.base_save_dir + File.separator + "test2.zip";
 
           zipFileUp(srcFile, dest_zipfile_name);
      }
@@ -26,6 +25,8 @@ public class FilesZipper {
      //zip multiple files up.
      public static void zipFilesUp(List<String> srcFiles, String dest_zipfile_name) throws IOException {
           FileOutputStream fos = new FileOutputStream(dest_zipfile_name);
+
+          M2SSystem.println( "dest_zipfile_name = " + dest_zipfile_name );
 
           ZipOutputStream zipOut = new ZipOutputStream(fos);
           for (String srcFile : srcFiles) {
