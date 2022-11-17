856faeaf618670fe32128ba04c6020102d24f890
Karl Schipul <karl@3ringprototype.com>
2022-11-17 13:47:03 -0500

    CONV-3 - initial setup of Deck Commerce conversion unit test.

---
+++ src/test/java/com/example/MageOrderToDeckCSVTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.migrator.Config;
+import com.migrator.Mage2DeckOrdersCSV;
+import com.migrator.Mage2SFOrders;
+import com.migrator.ReadFromFile;
+
+/**
+ * Unit test for converting Magento orders JSON to Deck orders CSV.
+ */
+public class MageOrderToDeckCSVTest {
+
+     
+}
